ace5f3957ca41cf13763520f6b8fb782093ac411
xvburak <xvburak@vutbr.cz>
2022-12-06 13:57:08 +0100

    flex col

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -16,7 +16,7 @@
     </div>
 </div>
 
-<div class="bg-purple min-h-screen text-white items-center flex flex-col-reverse md:flex-row md:space-x-4 pt-20 md:pt-4">
+<div class="bg-purple min-h-screen text-white items-center flex flex-col md:flex-row md:space-x-4 pt-20 md:pt-4">
     <div class="copy py-4 px-4 md:px-8 w-full md:w-1/2 h-min">
         <p>{@html data.aboutTwo.textEn}</p>
     </div>
@@ -38,7 +38,7 @@
     
 </div>
 
-<div class="bg-purple min-h-screen sec text-white items-center flex flex-col-reverse md:flex-row md:space-x-4 pt-20 md:pt-4">
+<div class="bg-purple min-h-screen sec text-white items-center flex flex-col md:flex-row md:space-x-4 pt-20 md:pt-4">
     <div class="copy py-4 px-4 md:px-8 w-full md:w-1/2 h-min">
         <p>{@html data.aboutTwo.textCs}</p>
     </div>
